fd468ac22ea79782cfd0d35e9de8751b31d3006a
Nikesh Kumar <77344784+NikeshKr@users.noreply.github.com>
2022-08-15 20:36:52 +0530

    Update RealTime Face Recognition.py

--- RealTime Face Recognition.py
+++ RealTime Face Recognition.py
@@ -1,49 +1,40 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import cv2
-import os
-cascPath=os.path.dirname(cv2.__file__)+"/data/haarcascade_frontalface_default.xml"
-faceCascade = cv2.CascadeClassifier(cascPath)
-video_capture = cv2.VideoCapture(0)
+from os.path import dirname
+def main():
+    cascPath=f'{dirname(__file__)}\haarcascade_frontalface_default.xml'
+    faceCascade = cv2.CascadeClassifier(cascPath)
+    video_capture = cv2.VideoCapture(0)
 
-while True:
-    # Capture frame-by-frame
-    ret, frames = video_capture.read()
+    while True:
+        # Capture frame-by-frame
+    
+        ret, frames = video_capture.read()
+        
+        gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
 
-    gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.CASCADE_SCALE_IMAGE
+        )
 
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30),
-        flags=cv2.CASCADE_SCALE_IMAGE
-    )
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in faces:
+            if(len(faces)-1): cv2.rectangle(frames, (x, y), (x+w, y+h), (0, 0, 255), 2)
+            else: cv2.rectangle(frames, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-    # Draw a rectangle around the faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frames, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        # Display the resulting frame
+        cv2.imshow('Video', frames)
 
-    # Display the resulting frame
-    cv2.imshow('Video', frames)
+        if(cv2.waitKey(1) & 0xFF == ord('q')):
+            break
+    video_capture.release()
+    cv2.destroyAllWindows()
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-video_capture.release()
-cv2.destroyAllWindows()
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
+if(__name__=='__main__'):
+    main()
 
 
 
